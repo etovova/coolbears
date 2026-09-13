933693e2954e2de67c079f7c8f848824cf86f8fe
etovova <yauheni84@hotmail.com>
2026-09-13 18:37:29 +0300

    Configure CoolBears domain, TON Connect and Pinata verification

---
+++ config.js
@@ -0,0 +1,15 @@
+window.COOLBEARS_CONFIG = {
+  officialWebsite: 'https://coolbears-nfts.com',
+  network: 'mainnet',
+  priceTon: 7,
+  supply: 10000,
+  royaltyPercent: 7,
+  maxPerTransaction: 50,
+  revealDate: '2026-10-07',
+  collectionAddress: '',
+  mintContractAddress: '',
+  treasuryAddress: 'UQBuosyZXH1PI2RBxsUgsjbD6RnsVOMxtCaVKEKMZRpXF9m7',
+  royaltyAddress: 'UQBuosyZXH1PI2RBxsUgsjbD6RnsVOMxtCaVKEKMZRpXF9m7',
+  getgemsUrl: '',
+  demoMode: true
+};
